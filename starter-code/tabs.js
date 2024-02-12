5843afd440f946d7cdeddd89441fd662432b4a71
Anthony Climer <anthonyclimer0925@gmail.com>
2024-02-11 19:39:43 -0500

    I have set up the tab to go either left or right

--- starter-code/tabs.js
+++ starter-code/tabs.js
@@ -1,18 +1,14 @@
-const tabList = document.querySelector('[role="tablist"]');
-const tabs = tabList.querySelectorAll('[role="tab"]');
+const tabList = document.querySelector('[role="tablist"]')
+const tabs = tabList.querySelectorAll('[role="tab"]')
 
 tabList.addEventListener('keydown', (e) => {
-    console.log('Event',e)
+    const keydownLeft = 37;
+    const keydownRight = 39;
 
-    const keydownLeft= 37;
-    const keydownRight= 39;
+    // change the tabindex of the current tab to  -1
+    
+    // if I keydown right, I will go to the right
 
-    // change the tabindex of the current tab to -1
-    const currentTab = tabs.getAttribute('tabindex')
-    console.log('current tab',currentTab)
-    // if the right key is pushed, move to the right
-    if(keydownRight) {
-        
-    }
-    // if the left key is pushed, move to the left
+    // if I keydown left, I will go to the left
+
 })
