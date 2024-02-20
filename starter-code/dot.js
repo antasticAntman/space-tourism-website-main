0a9e607efe0e4372fcb882c8718c09b26a3a829c
Anthony Climer <anthonyclimer0925@gmail.com>
2024-02-20 17:10:02 -0500

    I have updated key features and allowed for it to press forward

--- starter-code/dot.js
+++ starter-code/dot.js
@@ -9,17 +9,22 @@ let currentDot = 0
 // We are going to set the current dotindex to -1, once we move left or right, we adjust the focus,
 // and set the current dotindex to 0
 dotlist.addEventListener('keydown', (e)=>{
-    // console.log("event", e.keyCode)
     if(e.keyCode === keydownLeft || e.keyCode === keydownRight){
         dots[currentDot].setAttribute('dotindex', '-1')
-        console.log(dots.length)
         if(e.keyCode === keydownRight){
             if(currentDot >= dots.length-1){
                 currentDot = 0
             } else {
                 currentDot++
             }
+        } else {
+            if(currentDot <=0){
+                currentDot = dots.length-1
+            } else {
+                currentDot--
+            }
         }
     }
-    console.log("Current node",currentDot)
+    dots[currentDot].setAttribute('dotindex','0')
+    dots[currentDot].focus()
 })
