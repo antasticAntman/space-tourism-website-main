f67619297a6a7ea4413a6b248a4ef706088fd8dc
Anthony Climer <anthonyclimer0925@gmail.com>
2024-02-20 18:05:44 -0500

    I have the dots kind of working, the panels work, not the images!

--- starter-code/dot.js
+++ starter-code/dot.js
@@ -8,7 +8,40 @@ let currentDot = 0
 
 // We are going to set the current dotindex to -1, once we move left or right, we adjust the focus,
 // and set the current dotindex to 0
-dotlist.addEventListener('keydown', (e)=>{
+dotlist.addEventListener('keydown', changeDotFocus)
+
+// dotlist.addEventListener('click', (e) => {
+//     const targetDot = e.target
+//     console.log('click', targetDot)
+//     const targetPanel = targetDot.getAttribute('aria-control')
+//     const targetImage = targetDot.getAttribute('data-image')
+
+//     const dotContainer = targetDot.parentNode
+//     console.log('dotCOntainer', dotContainer)
+// })
+
+dots.forEach((dot)=>{
+    dot.addEventListener('click', (e) => {
+        console.log('click')
+        const targetDot = e.target
+        console.log('target',targetDot)
+        const targetPanel = targetDot.getAttribute('aria-controls')
+        // console.log('targetpanel',targetPanel)
+        const targetImage = targetDot.getAttribute('data-image')
+        const dotContainer = targetDot.parentNode
+        const mainDotContainer = dotContainer.parentNode
+
+        mainDotContainer.querySelectorAll('[role="dotpanel"]').forEach((panel)=> {
+            panel.setAttribute('hidden', true)
+        })
+
+        mainDotContainer.querySelector(`#${targetPanel}`).removeAttribute('hidden')
+
+        
+    })
+})
+
+function changeDotFocus(e){
     if(e.keyCode === keydownLeft || e.keyCode === keydownRight){
         dots[currentDot].setAttribute('dotindex', '-1')
         if(e.keyCode === keydownRight){
@@ -27,4 +60,4 @@ dotlist.addEventListener('keydown', (e)=>{
     }
     dots[currentDot].setAttribute('dotindex','0')
     dots[currentDot].focus()
-})
+}
