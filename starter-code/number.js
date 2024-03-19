c7a7b7dcaa2a694397845e29d67c786a816cc75a
Anthony Climer <anthonyclimer0925@gmail.com>
2024-03-19 19:49:15 -0400

    I have worked on the javascript and about half way finished

---
+++ starter-code/number.js
@@ -0,0 +1,37 @@
+const numberList = document.querySelector("[role='numberList']")
+const numbers= numberList.querySelectorAll("[role='number']")
+
+const keydownLeft=37;
+const keydownUp=38;
+const keydownRight=39;
+const keydownDown=40;
+
+let currentIndex = 0;
+numberList.addEventListener('keydown', highlightCurrentIndex)
+
+numbers.forEach( (num) => {
+    num.addEventListener('click', (e) => {
+    })
+})
+
+function highlightCurrentIndex (e) {
+    if(e.keyCode===keydownLeft || e.keyCode===keydownRight){
+        numbers[currentIndex].setAttribute('numberindex', '-1')
+        if(e.keyCode===keydownRight){
+            if(currentIndex >=numbers.length-1) {
+                currentIndex=0;
+            }else{
+                currentIndex++
+            }
+        } else if(e.keyCode===keydownLeft){
+            if(currentIndex===0){
+                currentIndex = numbers.length-1
+            }else{
+                currentIndex--
+            }
+        }
+        numbers[currentIndex].setAttribute('numberindex', '0')
+        numbers[currentIndex].focus()
+    }
+}
+
