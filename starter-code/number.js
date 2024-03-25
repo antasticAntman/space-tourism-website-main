fa5d83a65f96137e02cde25b6a84b6e7a291c7e2
Anthony Climer <anthonyclimer0925@gmail.com>
2024-03-25 17:13:53 -0400

    I have added a way to get rid of the image and add the image to the button

--- starter-code/number.js
+++ starter-code/number.js
@@ -1,10 +1,10 @@
 const numberList = document.querySelector("[role='numberList']")
+console.log('numberList', numberList)
 const numbers= numberList.querySelectorAll("[role='number']")
+console.log('numbers',numbers)
 
 const keydownLeft=37;
-const keydownUp=38;
 const keydownRight=39;
-const keydownDown=40;
 
 let currentIndex = 0;
 numberList.addEventListener('keydown', highlightCurrentIndex)
@@ -12,25 +12,31 @@ numberList.addEventListener('keydown', highlightCurrentIndex)
 numbers.forEach((num) => {
     num.addEventListener('click', (e) => {
         const selectedNum = e.target;
-        // console.log("prefered Number", selectedNum)
+        console.log("prefered Number", selectedNum)
         const selectedPanel = selectedNum.getAttribute("aria-controls")
-        // console.log('The Specific Panel is', selectedPanel)
+        console.log('The Specific Panel is', selectedPanel)
         const selectedImg = selectedNum.getAttribute("data-image")
         
         const numContainer = num.parentNode
         const ArticleContainer = numContainer.parentNode
         const parentContainer = ArticleContainer.parentNode
 
+        parentContainer.querySelectorAll("[role='num_img']").forEach((img) => {
+            console.log("img",img)
+            img.setAttribute('hidden', true)
+        })
+
         parentContainer.querySelectorAll("[role=num_panel]").forEach( (panel) => {
             console.log('Panels', panel)
             panel.setAttribute('aria-hidden', true)
         })
 
-        parentContainer.querySelector(`#${selectedPanel}`).setAttribute('aria-hidden', 'false')
+        parentContainer.querySelector(`#${selectedImg}`).removeAttribute('hidden')
+        parentContainer.querySelector(`#${selectedPanel}`).setAttribute('aria-hidden', false)
     })
 })
 
-function highlightCurrentIndex (e) {
+function highlightCurrentIndex(e){
     if(e.keyCode===keydownLeft || e.keyCode===keydownRight){
         numbers[currentIndex].setAttribute('numberindex', '-1')
         if(e.keyCode===keydownRight){
